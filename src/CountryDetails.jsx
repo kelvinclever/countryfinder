7581f8b859e6168cb094757cdfe23231a7bcd0ed
kelvinclever <kkelvinnzioka@gmail.com>
2023-05-26 11:05:01 +0300

    fixed changes

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import './CountryDetails.css'
-import {BsArrowLeft} from 'react-icons/Bs'
+import {BsArrowLeft} from 'react-icons/bs'
 
 const CountryDetails = ({ countries }) => {
   const { id } = useParams();
